1f1ccdb1621b3ca42a1f6ea8d64c0c4102c2847a
JulianSVC <santicastaneda099@gmail.com>
2025-02-18 10:57:21 -0500

    datos

--- src/Main.kt
+++ src/Main.kt
@@ -2,5 +2,10 @@ fun main() {
 
     println("Clase Kotlin")
 
+    println("Julian Santiago Venegas Castañeda")
+    println("Escuchar musica")
+    println("perrro:mickey")
+    println("EDAD:21")
+    println("DONDE_VIVE:CAJICA")
 
 }
